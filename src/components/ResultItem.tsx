33325a4fd1c3c6ca6a8d7138492efa9c4967868c
Vitaly Kukushkin <phoinixru@gmail.com>
2023-10-29 01:19:41 +0200

    feat: add empty components

---
+++ src/components/ResultItem.tsx
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+
+export default class ResultItem extends Component {
+  render() {
+    return <div>ResultItem</div>;
+  }
+}
---
+++ src/components/Search.tsx
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+
+export default class Search extends Component {
+  render() {
+    return <div className="search">Search</div>;
+  }
+}
