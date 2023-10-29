48de3f8b7bfc20cf350da5f1315412851875cde3
Vitaly Kukushkin <phoinixru@gmail.com>
2023-10-29 02:36:32 +0200

    feat: implement Search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { Component } from 'react';
 import './App.scss';
+import Results from './components/Results';
+import Search from './components/Search';
+import { PokemonCard } from './types/PokemonCard';
 
-function App() {
-  return <div>Hello world!</div>;
+type AppState = {
+  q: string;
+  results: PokemonCard[];
+};
+
+export default class App extends Component<null, AppState> {
+  state = {
+    q: '',
+    results: [],
+  };
+
+  render() {
+    const updateQuery = (q: string) => {
+      this.setState({ q });
+    };
+
+    return (
+      <div className="app">
+        <Search onChange={updateQuery} />
+        <Results />
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,37 @@
 import { Component } from 'react';
+import './Search.scss';
 
-export default class Search extends Component {
+type SearchProps = {
+  onChange: (q: string) => void;
+};
+
+type SearchState = {
+  value: string;
+};
+
+export default class Search extends Component<SearchProps, SearchState> {
+  state = {
+    value: localStorage?.q ?? '',
+  };
+
   render() {
-    return <div className="search">Search</div>;
+    const { onChange } = this.props;
+
+    const sendQuery = () => {
+      onChange(this.state.value);
+    };
+
+    return (
+      <div className="search">
+        <input
+          className="search__input"
+          onChange={(e) => this.setState({ value: e.target.value })}
+          value={this.state.value}
+        />
+        <button className="search__button" onClick={sendQuery}>
+          Search
+        </button>
+      </div>
+    );
   }
 }
