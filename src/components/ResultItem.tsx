a55568d6531de0a4f5cb0d24ab9805364b536a93
Vitaly Kukushkin <phoinixru@gmail.com>
2023-11-02 20:15:02 +0200

    refactor: ResultItem to functional component

--- src/components/ResultItem.tsx
+++ src/components/ResultItem.tsx
@@ -1,21 +1,19 @@
-import { Component } from 'react';
 import { PokemonCard } from '../types/PokemonCard';
 
 type ResultItemProps = {
   card: PokemonCard;
 };
 
-export default class ResultItem extends Component<ResultItemProps> {
-  render() {
-    const {
-      name,
-      images: { small },
-    } = this.props.card;
-    return (
-      <li className="card">
-        <h2 className="card__name">{name}</h2>
-        <img className="card__image" src={small} alt={name} />
-      </li>
-    );
-  }
+export default function ResultItem(props: ResultItemProps) {
+  const {
+    name,
+    images: { small },
+  } = props.card;
+
+  return (
+    <li className="card">
+      <h2 className="card__name">{name}</h2>
+      <img className="card__image" src={small} alt={name} />
+    </li>
+  );
 }
