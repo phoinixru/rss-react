cd6bec8f67f55445bf3970306f3644b2954e1856
Vitaly Kukushkin <phoinixru@gmail.com>
2023-11-03 19:57:13 +0200

    feat: let user change page size

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,5 +1,6 @@
 import { useSearchParams } from 'react-router-dom';
 import './Pagination.scss';
+import { PAGE_SIZES } from '../config';
 
 type PaginationProps = {
   page: number;
@@ -8,18 +9,25 @@ type PaginationProps = {
 };
 
 export default function Pagination(props: PaginationProps) {
-  const [, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const { page, pageSize, total } = props;
 
   const from = 1 + (page - 1) * pageSize;
-  const to = page * pageSize;
+  const to = Math.min(total, page * pageSize);
 
   const firstPage = 1;
   const lastPage = Math.ceil(total / pageSize);
 
   const changePage = (delta: number) => {
     const newPage = page + delta;
-    setSearchParams({ page: newPage.toString() });
+    searchParams.set('page', newPage.toString());
+    setSearchParams(searchParams);
+  };
+
+  const setPageSize = (pageSize: string) => {
+    if (pageSize) {
+      setSearchParams({ pageSize });
+    }
   };
 
   return (
@@ -42,6 +50,18 @@ export default function Pagination(props: PaginationProps) {
         >
           &gt;
         </button>
+
+        <select
+          onChange={(e) => setPageSize(e.target.value)}
+          defaultValue={pageSize}
+        >
+          <option value="">per page:</option>
+          {PAGE_SIZES.map((size) => (
+            <option key={size} value={size}>
+              {size}
+            </option>
+          ))}
+        </select>
       </fieldset>
     </div>
   );
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = 'https://api.pokemontcg.io/v2';
-export const PAGE_SIZES = [5, 10, 20, 40];
-export const DEFAULT_PAGE_SIZE = 5;
+export const PAGE_SIZES = [12, 24, 36, 48];
+export const DEFAULT_PAGE_SIZE = 12;
